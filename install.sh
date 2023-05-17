9ad72489f9a2868162f7efdb0afe88f8af6486f1
zhiqi.zhang <zhiqi.zhang@yitu-inc.com>
2023-05-17 13:53:13 +0800

    update

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zhiqizhiqi/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zhiqizhiqi/.tmux/master/install.sh)"
+
 git clone --depth=1 https://github.com/zhiqizhiqi/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
