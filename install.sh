3981a0c7f195f4e4e37ff462870455941dba9f1a
zhiqi.zhang <zhiqi.zhang@yitu-inc.com>
2023-05-17 14:00:33 +0800

    update

--- install.sh
+++ install.sh
@@ -1,10 +1,4 @@
 
-echo "clean zsh config"
-rm -rf $HOME/.oh-my-zsh/
-rm -f $HOME/.zshrc
-
-echo "install oh my zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zhiqizhiqi/ohmyzsh/master/tools/install.sh)"
 
 echo "clean tmux config"
 rm -rf $HOME/.tmux/
@@ -20,3 +14,10 @@ rm -f $HOME/.vimrc
 echo "install tmux config"
 git clone --depth=1 https://github.com/zhiqizhiqi/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+echo "clean zsh config"
+rm -rf $HOME/.oh-my-zsh/
+rm -f $HOME/.zshrc
+
+echo "install oh my zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zhiqizhiqi/ohmyzsh/master/tools/install.sh)"
